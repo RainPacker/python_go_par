3b87a34e27ed530b2973b001ddd67198ac96ab29
zhangyangyang <wb-zyy719219@antgroup.com>
2020-10-14 16:37:43 +0800

    更新优化

---
+++ helli.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=10;
+    int *p;
+    p=&a;
+    cout << p <<endl;
+    cout <<"2234"<< endl;
+    // cin >> a;
+    cout <<&a<< endl ;
+    return 0;
+}
